2863ec7cf7d4e50dea018a05bb5b140f9731f2d5
dogforever <162438228+dogforever@users.noreply.github.com>
2024-04-01 06:20:54 +0000

    feat: my first commit

---
+++ src/HelloWorldState.cs
@@ -0,0 +1,12 @@
+using AElf.Sdk.CSharp.State;
+
+namespace AElf.Contracts.HelloWorld
+{
+    // The state class is access the blockchain state
+    public class HelloWorldState : ContractState 
+    {
+        // A state that holds string value
+        public StringState Message { get; set; }
+        public Int64State Count { get; set; }
+    }
+}
